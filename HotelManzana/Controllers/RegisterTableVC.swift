6094ff5740bf1472f5bf2ec04bdc622482d4370c
Sharon Chetrit <sharon.chetrit@hotmail.fr>
2017-12-13 14:24:15 +0200

    hook up the registrationTableVC and delate all the extra stuff

---
+++ HotelManzana/Controllers/RegisterTableVC.swift
@@ -0,0 +1,23 @@
+//
+//  RegisterTableVC.swift
+//  HotelManzana
+//
+//  Created by Sharon Chetrit on 13/12/2017.
+//  Copyright © 2017 ronny abraham. All rights reserved.
+//
+
+import UIKit
+
+class RegisterTableVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+}
