c64ab2df24f93bd3f9fc22ee93652ee1fb47eb4b
Sharon Chetrit <sharon.chetrit@hotmail.fr>
2017-12-17 14:35:21 +0200

    my project

--- HotelManzana/Controllers/RegisterTableVC.swift
+++ HotelManzana/Controllers/RegisterTableVC.swift
@@ -12,9 +12,16 @@ class RegisterTableVC: UITableViewController {
     
     @IBOutlet weak var firstName: UITextField!
     
-    @IBOutlet weak var secondName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
     
-    @IBOutlet weak var thirdName: UITextField!
+    @IBOutlet weak var email: UITextField!
+    
+    @IBOutlet weak var checkInDateLbl: UILabel!
+    @IBOutlet weak var checkInDatePicker: UIDatePicker!
+    @IBOutlet weak var checkOutDateLbl: UILabel!
+    @IBOutlet weak var checkOutDatePicker: UIDatePicker!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +31,38 @@ class RegisterTableVC: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        let midnightToday = Calendar.current.startOfDay(for: Date())
+        
+        checkInDatePicker.minimumDate = midnightToday
+        checkInDatePicker.date = midnightToday
+        
+        checkOutDatePicker.minimumDate = checkInDatePicker.date.addingTimeInterval(86400)
     }
 
+    @IBAction func DoneBarButtonItem(_ sender: UIBarButtonItem) {
+        let firstN = firstName.text ?? ""
+        let lastN = lastName.text ?? ""
+        let mail = email.text ?? ""
+        
+        print("first name: \(firstN)")
+        print("last name: \(lastN)")
+        print("email: \(mail)")
+    }
+    
+ 
+    @IBAction func datePickerChanged(_ sender: UIDatePicker) {
+    }
+    
+    
+    
+    func updateDateView() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        
+        checkInDateLbl.text = dateFormatter.string(from: checkInDatePicker.date)
+        checkOutDateLbl.text = dateFormatter.string(from: checkOutDatePicker.date)
+        
+    }
+    
 }
